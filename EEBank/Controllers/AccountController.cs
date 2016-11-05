2b6602bd0888cc98ad15aa91105817872328e6a8
lissabet <lissa.johnas@gmail.com>
2016-11-05 19:06:03 +0300

    new changes

--- EEBank/Controllers/AccountController.cs
+++ EEBank/Controllers/AccountController.cs
@@ -153,9 +153,9 @@ namespace EEBank.Controllers
             {
                 var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
                 
+                
   
                 var result = await UserManager.CreateAsync(user, model.Password);
-                var usr = new Users { Email = user.Email, Password = model.Password };
                 if (result.Succeeded)
                 {
                     await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
